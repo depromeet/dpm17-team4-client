ad62a2d11b4c7758d77a24d46d217b3710a81255
SeieunYoo <isho48239@gmail.com>
2025-09-07 02:51:04 +0900

    feat: timeout 시간 조정

--- web/src/lib/api-client.ts
+++ web/src/lib/api-client.ts
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
-// Axios 인스턴스 생성
 const apiClient = axios.create({
   //TODO: server url 설정
   baseURL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000',
-  timeout: 10000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// 요청 인터셉터
 apiClient.interceptors.request.use(
   (config) => {
     //TODO: Access token 추가
@@ -22,7 +20,6 @@ apiClient.interceptors.request.use(
   }
 );
 
-// 응답 인터셉터
 apiClient.interceptors.response.use(
   (response) => {
     return response;
