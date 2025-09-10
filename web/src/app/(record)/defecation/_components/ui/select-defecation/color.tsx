955f40228b1f80c2f1456940cbe4a7bb6a9d174d
dvp-tae <th3016@naver.com>
2025-09-11 00:37:09 +0900

    fix: biome CI 오류 수정

--- web/src/app/(record)/defecation/_components/ui/select-defecation/color.tsx
+++ web/src/app/(record)/defecation/_components/ui/select-defecation/color.tsx
@@ -5,7 +5,7 @@ import { DEFECATION_TRY_COLOR } from '../../constants/description';
 import { useDefecation } from '../../providers/defecation-providers';
 
 export default function Color() {
-	const { defecationState, setDefecationState } = useDefecation();
+  const { defecationState, setDefecationState } = useDefecation();
 
   return (
     <div className="flex items-center justify-between gap-[12.6px] max-[398px]:gap-1">
@@ -16,13 +16,13 @@ export default function Color() {
             setDefecationState({ ...defecationState, selectedColor: key });
           }}
           key={key}
-          className="flex flex-col items-center justify-center gap-2.5 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#5170FF]"
+          className="flex flex-col items-center justify-center gap-2.5"
         >
           <div
-            className={cn(
-              'w-10 h-10 rounded-[10px] border border-transparent',
-              defecationState.selectedColor === key && 'border-white'
-            )}
+              className={cn(
+                  'w-10 h-10 rounded-[10px] border border-transparent',
+                  defecationState.selectedColor === key && 'border-white'
+              )}
             style={{ backgroundColor: value[1] }}
           />
           <div className="text-sm font-normal">{value[0]}</div>
