8a4428e36275fe160173eac320cb2df9b9e30bb7
dvp-tae <th3016@naver.com>
2025-09-14 04:12:29 +0900

    fix: 벡터 데이터 속성 상수명 변경 및 주석 추가

--- web/src/components/icons/Chevron.tsx
+++ web/src/components/icons/Chevron.tsx
@@ -15,7 +15,8 @@ export const ChevronIcon = ({
     down: 'rotate-0',
   };
 
-  const d =
+  // NOTE(taehyeon): 벡터 데이터
+  const DATA =
     'M13.1994 5.80857C13.3779 5.62358 13.3779 5.32373 13.1994 5.13874C13.0208 4.95375 12.7315 4.95375 12.5529 5.13874L7.99992 9.85652L3.44691 5.13874C3.26839 4.95375 2.97901 4.95375 2.80048 5.13874C2.62195 5.32373 2.62195 5.62358 2.80048 5.80857L7.6767 10.8613C7.85523 11.0462 8.14461 11.0462 8.32313 10.8613L13.1994 5.80857Z';
 
   return (
@@ -35,7 +36,7 @@ export const ChevronIcon = ({
       {...props}
     >
       <path
-        d={d}
+        d={DATA}
         fill={'currentColor'}
         stroke={'currentColor'}
         strokeWidth={strokeWidth}
