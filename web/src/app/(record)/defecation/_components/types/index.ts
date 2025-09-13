d0b0bf6bd5e7ae051ebe0b023baf216c877d289f
dvp-tae <th3016@naver.com>
2025-09-13 19:41:40 +0900

    fix: 주석 컨벤션 리뷰 반영

--- web/src/app/(record)/defecation/_components/types/index.ts
+++ web/src/app/(record)/defecation/_components/types/index.ts
@@ -1,10 +1,10 @@
-// 배변 기록 폼 타입
+// NOTE(taehyeon): 배변 기록 폼 타입
 export interface DefecationState {
-  selectedWhen: Date; // 배변 시각
-  selectedTry: string; // 배변 시도
-  selectedColor: string; // 배변 색상
-  selectedShape: string; // 배변 모양
-  selectedPain: string; // 배변 통증
-  selectedTimeTaken: string; // 배변 소요 시간
-  selectedOptional: string; // 배변 특이 사항
+  selectedWhen: Date;
+  selectedTry: string;
+  selectedColor: string;
+  selectedShape: string;
+  selectedPain: string;
+  selectedTimeTaken: string;
+  selectedOptional: string;
 }
