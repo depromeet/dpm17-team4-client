b05220396952e6a069f954e9fca32e86b562bc6e
dvp-tae <th3016@naver.com>
2025-09-14 03:55:47 +0900

    chore: 주석 추가

--- web/src/components/icons/icon.type.ts
+++ web/src/components/icons/icon.type.ts
@@ -1,6 +1,13 @@
+/**
+ * @param size
+ * @param strokeWidth
+ * @param color
+ * @param className
+ * @returns
+ */
+
 export interface IconProps extends React.SVGProps<SVGSVGElement> {
   size?: string;
   strokeWidth?: string;
-  color?: string;
   className?: string;
 }
