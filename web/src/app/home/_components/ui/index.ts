bc667cc0313b88dfef363f8afa9197dfbe798a74
ppinppini <bnm0218@naver.com>
2025-09-16 17:24:37 +0900

    feat: BottomNavigation 컴포넌트 구현
    - 하단 네비게이션 바 컴포넌트 추가
    - 홈, 캘린더, 리포트, 마이 탭 구성
    - 현재 활성 탭 상태 관리
    - 반응형 디자인 적용

--- web/src/app/home/_components/ui/index.ts
+++ web/src/app/home/_components/ui/index.ts
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
+=======
+export { default as BottomNavigation } from './BottomNavigation';
+export { default as RecordButton } from './RecordButton';
+>>>>>>> c754ef7 (feat: BottomNavigation 컴포넌트 구현)
 export { default as RecordSection } from './RecordSection';
