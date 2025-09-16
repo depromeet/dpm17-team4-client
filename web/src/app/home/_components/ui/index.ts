72be34d7784d07af7cc189c6a806e472c4ba5806
ppinppini <bnm0218@naver.com>
2025-09-16 18:01:19 +0900

    refactor: home-ui에서 BottomNavigation 관련 코드 제거

--- web/src/app/home/_components/ui/index.ts
+++ web/src/app/home/_components/ui/index.ts
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 export { default as BottomNavigation } from './BottomNavigation';
 <<<<<<< HEAD
@@ -6,4 +7,9 @@ export { default as RecordButton } from './RecordButton';
 >>>>>>> c754ef7 (feat: BottomNavigation 컴포넌트 구현)
 =======
 >>>>>>> feccaa5 (fix: RecordSection import 경로 수정 및 사용하지 않는 변수 정리)
+=======
+export { default as BottomNavigation } from './BottomNavigation';
+=======
+>>>>>>> 12ce55f (refactor: home-ui에서 BottomNavigation 관련 코드 제거)
+>>>>>>> 1d431d7 (refactor: home-ui에서 BottomNavigation 관련 코드 제거)
 export { default as RecordSection } from './RecordSection';
