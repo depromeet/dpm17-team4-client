d8d4c965b1cfb2ccde448861b3fa42c235a345fd
ppinppini <bnm0218@naver.com>
2025-09-16 18:18:33 +0900

    fix: RecordSection import 경로 수정 및 사용하지 않는 변수 정리

--- web/src/app/home/_components/ui/RecordSection.tsx
+++ web/src/app/home/_components/ui/RecordSection.tsx
@@ -4,6 +4,10 @@ import ChevronLeft from '@/assets/home/IC_Chevron_Left.png';
 import ChevronRight from '@/assets/home/IC_Chevron_Right.png';
 import { cn } from '@/utils/utils-cn';
 import { formatDate, isNextDisabled, isPrevDisabled } from '../utils/util-date';
+<<<<<<< HEAD
+=======
+import { getRecordPath } from '../utils/util-route';
+>>>>>>> feccaa5 (fix: RecordSection import 경로 수정 및 사용하지 않는 변수 정리)
 
 type RecordSectionProps = {
   navHeight: number;
@@ -21,11 +25,19 @@ const RecordSection = ({ navHeight }: RecordSectionProps) => {
     }
     setSelectedDate(newDate);
   };
+<<<<<<< HEAD
   // NOTE(yubin): RecordButton 브렌치 병합시 주석해제
   // const handleRecordClick = (type: 'defecation' | 'lifestyle') => {
   //   const path = getRecordPath(type, selectedDate);
   //   router.push(path);
   // };
+=======
+
+  const _handleRecordClick = (type: 'defecation' | 'lifestyle') => {
+    const path = getRecordPath(type, selectedDate);
+    router.push(path);
+  };
+>>>>>>> feccaa5 (fix: RecordSection import 경로 수정 및 사용하지 않는 변수 정리)
 
   return (
     <section
@@ -63,8 +75,24 @@ const RecordSection = ({ navHeight }: RecordSectionProps) => {
           />
         </button>
       </div>
+      {/* RecordButton 컴포넌트는 record-button 브랜치에서 관리됩니다 */}
       <div className="flex gap-[0.69rem] mt-4">
+<<<<<<< HEAD
         {/* NOTE(yubin): RecordButton 컴포넌트 추가  */}
+=======
+        <div className="flex items-start bg-gray-800 flex-1 p-[1.12rem] rounded-[0.875rem] text-body3-r">
+          <div className="text-left flex flex-col flex-1">
+            <div className="text-gray-400">이날의 뿡</div>
+            <div className="text-body1-sb">배변 기록하기</div>
+          </div>
+        </div>
+        <div className="flex items-start bg-gray-800 flex-1 p-[1.12rem] rounded-[0.875rem] text-body3-r">
+          <div className="text-left flex flex-col flex-1">
+            <div className="text-gray-400">이날의 냠</div>
+            <div className="text-body1-sb">생활 기록하기</div>
+          </div>
+        </div>
+>>>>>>> feccaa5 (fix: RecordSection import 경로 수정 및 사용하지 않는 변수 정리)
       </div>
     </section>
   );
--- web/src/app/home/_components/ui/index.ts
+++ web/src/app/home/_components/ui/index.ts
@@ -1,6 +1,9 @@
 <<<<<<< HEAD
 =======
 export { default as BottomNavigation } from './BottomNavigation';
+<<<<<<< HEAD
 export { default as RecordButton } from './RecordButton';
 >>>>>>> c754ef7 (feat: BottomNavigation 컴포넌트 구현)
+=======
+>>>>>>> feccaa5 (fix: RecordSection import 경로 수정 및 사용하지 않는 변수 정리)
 export { default as RecordSection } from './RecordSection';
