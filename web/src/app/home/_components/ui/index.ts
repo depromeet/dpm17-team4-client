0a0b241e0d236f110400b0f351f82286e1164c92
ppinppini <bnm0218@naver.com>
2025-09-18 22:37:11 +0900

    fix: 충돌 마커 제거 및 Biome 오류 수정

--- web/src/app/home/_components/ui/index.ts
+++ web/src/app/home/_components/ui/index.ts
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 export { default as BottomNavigation } from './BottomNavigation';
-<<<<<<< HEAD
 export { default as RecordButton } from './RecordButton';
->>>>>>> c754ef7 (feat: BottomNavigation 컴포넌트 구현)
-=======
->>>>>>> feccaa5 (fix: RecordSection import 경로 수정 및 사용하지 않는 변수 정리)
-=======
-=======
->>>>>>> 6e29818 (feat: BottomNavigation 컴포넌트 구현)
-export { default as BottomNavigation } from './BottomNavigation';
-=======
->>>>>>> 12ce55f (refactor: home-ui에서 BottomNavigation 관련 코드 제거)
->>>>>>> 1d431d7 (refactor: home-ui에서 BottomNavigation 관련 코드 제거)
 export { default as RecordSection } from './RecordSection';
-=======
-export { default as BottomNavigation } from "./BottomNavigation";
-export { default as RecordButton } from "./RecordButton";
-export { default as RecordSection } from "./RecordSection";
->>>>>>> 91e1240 (feat: BottomNavigation 컴포넌트 구현)
