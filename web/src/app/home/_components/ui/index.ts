7fadf7f67825b0fd8fae30af79e50a911026f392
ppinppini <bnm0218@naver.com>
2025-09-18 22:40:37 +0900

    fix:build error

--- web/src/app/home/_components/ui/index.ts
+++ web/src/app/home/_components/ui/index.ts
@@ -1,3 +1,2 @@
 export { default as BottomNavigation } from './BottomNavigation';
-export { default as RecordButton } from './RecordButton';
 export { default as RecordSection } from './RecordSection';
