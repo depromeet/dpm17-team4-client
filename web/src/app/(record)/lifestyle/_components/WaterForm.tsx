b7138a29e427bafc90682201700ca786ab329fd6
SeieunYoo <isho48239@gmail.com>
2025-09-21 02:31:17 +0900

    feat: water form 에도 상태 추가

--- web/src/app/(record)/lifestyle/_components/WaterForm.tsx
+++ web/src/app/(record)/lifestyle/_components/WaterForm.tsx
@@ -1,12 +1,15 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent } from 'react';
 
 const TOTAL_STEPS = 10;
 
-export const WaterForm = () => {
-  const [currentStep, setCurrentStep] = useState(0);
+interface WaterFormProps {
+  waterCups: number;
+  setWaterCups: (cups: number) => void;
+}
 
+export const WaterForm = ({ waterCups, setWaterCups }: WaterFormProps) => {
   const handleSliderChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setCurrentStep(parseInt(e.target.value));
+    setWaterCups(parseInt(e.target.value));
   };
 
   return (
@@ -18,7 +21,7 @@ export const WaterForm = () => {
 
         <div className="flex flex-col items-center">
           {/* Current Value Display */}
-          <div className="text-h3 text-white mb-[1rem]">{currentStep}잔</div>
+          <div className="text-h3 text-white mb-[1rem]">{waterCups}잔</div>
 
           {/* Progress Bar Container */}
           <div className="relative w-full max-w-[20rem]">
@@ -27,7 +30,7 @@ export const WaterForm = () => {
               type="range"
               min="0"
               max={TOTAL_STEPS}
-              value={currentStep}
+              value={waterCups}
               onChange={handleSliderChange}
               className="w-full h-[1.875rem] rounded-[62.4375rem] appearance-none cursor-pointer px-[0.25rem] py-[0.38rem] bg-gradient-to-r from-[#52DFDB] to-[#008CFF] [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-6 [&::-webkit-slider-thumb]:h-6 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-white [&::-webkit-slider-thumb]:shadow-md [&::-moz-range-thumb]:w-6 [&::-moz-range-thumb]:h-6 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-white [&::-moz-range-thumb]:cursor-pointer [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-white [&::-moz-range-thumb]:shadow-md [&::-moz-range-thumb]:appearance-none"
             />
