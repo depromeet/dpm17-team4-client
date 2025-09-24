f5cc9affb9cd7b39ea9edd722161fed4cd526309
ppinppini <bnm0218@naver.com>
2025-09-24 17:31:08 +0900

    feat: navigation 높이,현재 탭 과 같은 ㅅ상태 전역 context로 주입

---
+++ web/src/app/(with-bottom-navigation)/home/_components/ui/BottomNavigation.tsx
@@ -0,0 +1,62 @@
+import type { RefObject } from 'react';
+import { CalendarIcon, HomeIcon, ReportIcon, UserIcon } from '@/components';
+import { useNavigationContext } from '@/contexts/NavigationContext';
+import { cn } from '@/utils/utils-cn';
+
+const BOTTOM_NAV_TABS = [
+  {
+    id: 'home' as const,
+    name: '홈',
+    icon: HomeIcon,
+  },
+  {
+    id: 'calendar' as const,
+    name: '캘린더',
+    icon: CalendarIcon,
+  },
+  {
+    id: 'report' as const,
+    name: '리포트',
+    icon: ReportIcon,
+  },
+  {
+    id: 'my' as const,
+    name: '마이',
+    icon: UserIcon,
+  },
+];
+
+type BottomNavigationProps = {
+  navRef: RefObject<HTMLElement | null>;
+};
+
+const BottomNavigation = ({ navRef }: BottomNavigationProps) => {
+  const { handleTabClick, currentTab } = useNavigationContext();
+
+  return (
+    <nav ref={navRef} className="bg-gray-900 fixed bottom-0 border-none w-full">
+      <div className="flex justify-between">
+        {BOTTOM_NAV_TABS.map((tab) => {
+          const IconComponent = tab.icon;
+          return (
+            <button
+              type="button"
+              key={tab.id}
+              onClick={() => handleTabClick(tab.id)}
+              className="py-[1.19rem] pl-[1.78rem] pr-[1.84rem] cursor-pointer flex flex-col items-center gap-1 bg-transparent border-none text-white"
+            >
+              <IconComponent
+                className={cn(
+                  currentTab === tab.id ? 'text-white' : 'text-gray-500'
+                )}
+              />
+              {tab.name}
+            </button>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
+
+export default BottomNavigation;
