fe444a70eab0b8122d6f3e5931ff21426b63d9f1
ppinppini <bnm0218@naver.com>
2025-09-24 16:17:20 +0900

    fix: icons 컴포넌트의 delete icon 경로추가

--- web/src/components/icons/index.ts
+++ web/src/components/icons/index.ts
@@ -2,6 +2,7 @@ export * from './Arrow';
 export * from './Calendar';
 export * from './Chevron';
 export * from './ChevronThin';
+export * from './Delete';
 export * from './Home';
 export * from './Report';
 export * from './User';
