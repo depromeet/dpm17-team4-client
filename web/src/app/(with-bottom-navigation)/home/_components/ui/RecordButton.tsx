1ee8f61bbd375475f1a2a577de77ab870c9c368a
ppinppini <bnm0218@naver.com>
2025-09-24 17:30:25 +0900

    feat: props 로 icon 동적으로 주입

---
+++ web/src/app/(with-bottom-navigation)/home/_components/ui/RecordButton.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import type { ButtonHTMLAttributes } from "react";
+
+type RecordButtonProps = {
+	title: string;
+	subtitle: string;
+	icon: string;
+} & Omit<ButtonHTMLAttributes<HTMLButtonElement>, "type">;
+
+const RecordButton = ({
+	title,
+	subtitle,
+	icon,
+	...buttonProps
+}: RecordButtonProps) => {
+	return (
+		<button
+			className="flex items-start bg-gray-800 flex-1 p-[1.12rem] rounded-[0.875rem] text-body3-r cursor-pointer"
+			type="button"
+			{...buttonProps}
+		>
+			<div className="text-left flex flex-col flex-1">
+				<div className="text-gray-400">{title}</div>
+				<div className="text-body1-sb">{subtitle}</div>
+				<div className="flex justify-end">
+					<Image src={icon} alt="아이콘" width={24} height={24} />
+				</div>
+			</div>
+		</button>
+	);
+};
+
+export default RecordButton;
