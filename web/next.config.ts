42a881fd6cedd0f12ba4f714a6d555c7399c1fe8
onlyshyun <mn031621019@gmail.com>
2025-10-11 20:00:46 +0900

    fix(web): setting

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   allowedDevOrigins: ['cushionlike-shallowly-nancie.ngrok-free.dev'],
   experimental: {
     serverActions: {
