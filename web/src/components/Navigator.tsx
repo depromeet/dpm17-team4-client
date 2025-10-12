2230e1a0ca4c5392b8c3deb5d805edbbaf21ee24
SeieunYoo <isho48239@gmail.com>
2025-10-13 00:40:31 +0900

    refactor: clean up formatting and improve readability in LifeStyleNavigator and Navigator components

--- web/src/components/Navigator.tsx
+++ web/src/components/Navigator.tsx
@@ -23,34 +23,28 @@ interface NavigatorCenterProps {
   children?: ReactNode;
   className?: string;
 }
-    
-const Left = ({ children, className = "" }: NavigatorLeftProps) => (
-  <div className={`flex items-center ${className}`}>
-    {children}
-  </div>
+
+const Left = ({ children, className = '' }: NavigatorLeftProps) => (
+  <div className={`flex items-center ${className}`}>{children}</div>
 );
 
-const Right = ({ children, className = "" }: NavigatorRightProps) => (
-  <div className={`flex items-center justify-end ${className}`}>
-    {children}
-  </div>
+const Right = ({ children, className = '' }: NavigatorRightProps) => (
+  <div className={`flex items-center justify-end ${className}`}>{children}</div>
 );
 
-const Center = ({ children, className = "" }: NavigatorCenterProps) => (
-  <span className={`text-h3 ${className}`}>
-    {children}
-  </span>
+const Center = ({ children, className = '' }: NavigatorCenterProps) => (
+  <span className={`text-h3 ${className}`}>{children}</span>
 );
 
-const NavigatorComponent = ({ children, className = "" }: NavigatorProps) => {
+const NavigatorComponent = ({ children, className = '' }: NavigatorProps) => {
   const router = useRouter();
 
   return (
-    <div className={`fixed top-0 left-0 w-full h-[56px] z-10 bg-gray-900 text-white p-4 flex shrink-0 ${className}`}>
+    <div
+      className={`fixed top-0 left-0 w-full h-[56px] z-10 bg-gray-900 text-white p-4 flex shrink-0 ${className}`}
+    >
       <ArrowIcon type="left" className="w-6 h-6" onClick={router.back} />
-      <div className="flex-1 flex justify-center items-center">
-        {children}
-      </div>
+      <div className="flex-1 flex justify-center items-center">{children}</div>
     </div>
   );
 };
