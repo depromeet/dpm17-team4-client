4797edc15b26f06e581d6b3d81451ec1677189ac
SeieunYoo <isho48239@gmail.com>
2025-10-12 20:46:38 +0900

    feat(web): export delete mutation hook for activity records
    - Added export for `useActivityRecordDeleteMutation` to facilitate the deletion of activity records in the application.

--- web/src/hooks/mutations/index.ts
+++ web/src/hooks/mutations/index.ts
@@ -1,3 +1,4 @@
+export { useActivityRecordDeleteMutation } from './useActivityRecordDeleteMutation';
 export { useActivityRecordMutation } from './useActivityRecordMutation';
 export { useActivityRecordUpdateMutation } from './useActivityRecordUpdateMutation';
 export { useRecordMutation } from './useRecordMutation';
