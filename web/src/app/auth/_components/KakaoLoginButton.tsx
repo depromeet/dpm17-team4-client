5d1b256289d0802054590230b96703b0f6c3b583
ppinppini <bnm0218@naver.com>
2025-10-12 21:32:01 +0900

    feat(web): 말풍선 컴포넌트 UI 추가

--- web/src/app/auth/_components/KakaoLoginButton.tsx
+++ web/src/app/auth/_components/KakaoLoginButton.tsx
@@ -1,18 +1,24 @@
 'use client';
 import Image from 'next/image';
 import kakaoSymbol from '@/assets/auth/kakao_shape 2.svg';
+import { SpeechBubble } from '@/components/icons/SpeechBubble';
 
 const KakaoLoginButton = () => {
   return (
-    <button
-      type="submit"
-      className="flex w-[21.4375rem] h-[3.5rem] px-6 items-center gap-1 flex-shrink-0 rounded-[0.625rem] bg-[#FEE500] text-black hover:bg-[#E6CF00] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 transition-colors"
-    >
-      <Image src={kakaoSymbol} alt="" width={24} height={24} aria-hidden />
-      <span className="text-button-2 font-semibold flex-1 justify-center">
-        카카오톡으로 시작하기
-      </span>
-    </button>
+    <div className="relatvie">
+      <SpeechBubble className="translate-y-[0.47rem] translate-x-[15.5rem]">
+        가장 간편!
+      </SpeechBubble>
+      <button
+        type="submit"
+        className="flex w-[21.4375rem] h-[3.5rem] px-6 items-center gap-1 flex-shrink-0 rounded-[0.625rem] bg-[#FEE500] text-black hover:bg-[#E6CF00] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 transition-colors"
+      >
+        <Image src={kakaoSymbol} alt="" width={24} height={24} aria-hidden />
+        <span className="text-button-2 font-semibold flex-1 justify-center">
+          카카오톡으로 시작하기
+        </span>
+      </button>
+    </div>
   );
 };
 
