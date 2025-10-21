c01aa4e850b2c44674974eb82b87db9181a2d20b
유세은 <101736358+SeieunYoo@users.noreply.github.com>
2025-10-22 00:25:20 +0900

    feat: Add LogoutModal component for user logout confirmation

---
+++ web/src/app/my/profile/_components/LogoutModal.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Button } from '@/components/Button';
+import { Modal } from '@/components/Modal';
+import { PAGE_ROUTES } from '@/constants';
+import { useUserInfo } from '@/hooks';
+
+interface LogoutModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const LogoutModal = ({ isOpen, onClose }: LogoutModalProps) => {
+  const { handleLogOut } = useUserInfo();
+  const router = useRouter();
+
+  const handleLogoutClick = () => {
+    handleLogOut();
+    router.push(PAGE_ROUTES.AUTH);
+  };
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="mb-7 flex-col">
+        <h1 className="text-body1-m text-white text-center">
+          로그아웃하시겠어요?
+        </h1>
+      </div>
+      <div className="flex gap-2">
+        <Button className="!bg-gray-700 flex-1" onClick={onClose}>
+          취소
+        </Button>
+        <Button
+          className="bg-purple-600 flex-1 hover:bg-purple-700"
+          onClick={handleLogoutClick}
+        >
+          로그아웃
+        </Button>
+      </div>
+    </Modal>
+  );
+};
