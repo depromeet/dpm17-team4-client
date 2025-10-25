084e4adea3eb9400ca99b4d0787ace4f62b666e3
유세은 <101736358+SeieunYoo@users.noreply.github.com>
2025-10-25 16:45:02 +0900

    feat: usequery 로

--- web/src/hooks/queries/useUserMeQuery.ts
+++ web/src/hooks/queries/useUserMeQuery.ts
@@ -1,14 +1,14 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { userApi } from '@/apis/userApi';
 import { QUERY_KEYS } from '@/constants';
 import type { UserData } from '@/types/dto/user.dto';
 
 export const useUserMeQuery = () => {
-  return useSuspenseQuery<UserData>({
+  return useQuery<UserData>({
     queryKey: QUERY_KEYS.USER_ME,
     queryFn: async () => {
       const response = await userApi.getMe();
-      return response.data; // API 응답의 data 필드만 반환
+      return response.data;
     },
   });
 };
