38b36552db87f3a0425d5f5631662da2a8e7f7ee
유세은 <101736358+SeieunYoo@users.noreply.github.com>
2025-10-26 20:12:16 +0900

    feat: report query 수정

--- web/src/hooks/queries/useReportQuery.ts
+++ web/src/hooks/queries/useReportQuery.ts
@@ -3,11 +3,15 @@ import { reportApi } from '@/apis/reportApi';
 import { QUERY_KEYS } from '@/constants';
 import type { ReportDataResponseDto } from '@/types/dto/report.dto';
 
-export const useReportQuery = () => {
+interface UseReportQueryParams {
+  dateTime?: string; // YYYY-MM-DD 형태의 날짜 문자열
+}
+
+export const useReportQuery = (params?: UseReportQueryParams) => {
   return useQuery<ReportDataResponseDto>({
     queryKey: QUERY_KEYS.REPORT,
     queryFn: async () => {
-      const response = await reportApi.reportDailyData();
+      const response = await reportApi.reportDailyData(params?.dateTime);
       return response.data.data;
     },
     refetchOnMount: 'always',
