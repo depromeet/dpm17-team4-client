d97a1e3738ca052d4cca31e01675544afb57e318
유세은 <101736358+SeieunYoo@users.noreply.github.com>
2025-10-26 20:14:39 +0900

    feat: record 는 원복

--- web/src/apis/recordApi.ts
+++ web/src/apis/recordApi.ts
@@ -1,23 +1,16 @@
 import { API_ENDPOINTS } from '@/constants';
 import apiClient from '@/lib/api-client';
-import type { ReportDataResponseDto } from '@/types/dto/report.dto';
+import type {
+  RecordDataRequestDto,
+  RecordDataResponseDto,
+} from '@/types/dto/record.dto';
 
-interface ApiResponse<T> {
-  status: number;
-  message: string;
-  data: T;
-}
-
-export const reportApi = {
-  reportDailyData: (dateTime?: string) => {
-    if (dateTime) {
-      const localDateTime = `${dateTime}T00:00:00`;
-      const url = `${API_ENDPOINTS.REPORT.BASE}?dateTime=${localDateTime}`;
-      return apiClient.get<ApiResponse<ReportDataResponseDto>>(url);
-    }
-
-    return apiClient.get<ApiResponse<ReportDataResponseDto>>(
-      API_ENDPOINTS.REPORT.BASE
+export const recordApi = {
+  recordDailyData: async (data: RecordDataRequestDto) => {
+    const response = await apiClient.post<RecordDataResponseDto>(
+      API_ENDPOINTS.RECORD.BASE,
+      data
     );
+    return response.data;
   },
 };
