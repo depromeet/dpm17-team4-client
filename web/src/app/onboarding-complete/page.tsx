ef47f4f69fae791fa870f323eaf5dd2a9cb2c455
dvp-tae <th3016@naver.com>
2025-11-01 17:04:40 +0900

    feat(web): 온보딩 완료 페이지 연결

--- web/src/app/onboarding-complete/page.tsx
+++ web/src/app/onboarding-complete/page.tsx
@@ -2,10 +2,10 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import CheckPng from '@/assets/defecation/check.png';
-import CompletePng from '@/assets/defecation/complete-succes.png';
 import { BottomBtnBar } from '@/components';
 import { PAGE_ROUTES } from '@/constants';
+import CheckPng from '@/assets/defecation/check.png';
+import CompletePng from '@/assets/defecation/complete-succes.png';
 
 export default function OnboardingCompletePage() {
   const router = useRouter();
@@ -43,20 +43,13 @@ export default function OnboardingCompletePage() {
 
         {/* 메인 텍스트 */}
         <div className="text-center mb-8">
-          <h1 className="text-[1.5rem] font-semibold mb-2.5">
-            회원가입이 완료되었어요
-          </h1>
+          <h1 className="text-[1.5rem] font-semibold mb-2.5">회원가입이 완료되었어요</h1>
           <div className="text-[#707885] text-body3-m leading-relaxed">
             <p>지금 바로 함께 기록하고 관리해봐요</p>
           </div>
         </div>
       </div>
-
-      <BottomBtnBar
-        onSubmit={handleGoHome}
-        text={'홈으로'}
-        className="bg-transparent"
-      />
+      <BottomBtnBar onSubmit={handleGoHome} text={'홈으로'} className='bg-transparent'/>
     </div>
   );
 }
