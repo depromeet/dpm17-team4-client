9b4e3d0f87458fff860a542b34a122cfa04e5fb7
황유빈 <97329194+ppinppini@users.noreply.github.com>
2025-11-07 16:59:48 +0900

    feat(web): 배변점수,스트레스 점수 분석 차트 UI (#133)
    ## 의도 🔍
    - 주간,월간의 사용된 배변 점수,스트레스 점수 분석 차트 UI 를 구현합니다.
    
    ## 작업 결과 📸 (Optional)
    <img width="353" height="430" alt="스크린샷 2025-11-07 오후 4 46 11"
    src="https://github.com/user-attachments/assets/8444f37d-38b3-4c92-82c7-5ef68018d8e3"
    />
    <img width="360" height="490" alt="스크린샷 2025-11-07 오후 4 46 21"
    src="https://github.com/user-attachments/assets/eb169a5f-ba63-4ce6-8153-072051ffcdd8"
    />
    
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## 릴리스 노트
    
    * **새로운 기능**
      * 배변 점수 차트 시각화 추가
      * 스트레스 분석 데이터를 표시하는 새로운 차트 기능 추가
      * 보고서 섹션의 차트 디스플레이 개선 및 최적화
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ web/src/app/(with-bottom-navigation)/report/weekly/_components/StressAnalysisChart.tsx
@@ -0,0 +1,222 @@
+'use client';
+
+import type { ApexOptions } from 'apexcharts';
+import dynamic from 'next/dynamic';
+import Image from 'next/image';
+import streching from '@/assets/report/stretching.png';
+
+const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+export type StressAnalysisChart = {
+  message: string;
+  image: string;
+  items: {
+    day: string;
+    stress: string | null;
+  }[];
+};
+
+interface StressAnalysisChartProps {
+  stressAnalysis: StressAnalysisChart;
+  xLabels: string[];
+  displayLabels: string[];
+}
+
+const stressLevelToValue: { [key: string]: number } = {
+  VERY_LOW: 0,
+  LOW: 25,
+  MEDIUM: 50,
+  HIGH: 75,
+  VERY_HIGH: 100,
+};
+
+export function StressAnalysisChart({
+  stressAnalysis,
+  xLabels,
+  displayLabels,
+}: StressAnalysisChartProps) {
+  const data = new Map(
+    stressAnalysis.items.map((item) => [
+      item.day,
+      item.stress ? stressLevelToValue[item.stress] : null,
+    ])
+  );
+
+  const chartData = xLabels
+    .map((day, index) => {
+      const value = data.get(day);
+      if (value !== undefined && value !== null) {
+        return { x: index, y: value };
+      }
+      return null;
+    })
+    .filter((item) => item !== null) as { x: number; y: number }[];
+
+  const series = [
+    {
+      name: '스트레스 분석',
+      data: chartData,
+    },
+  ];
+
+  const options: ApexOptions = {
+    colors: ['#796AFF'],
+    chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
+    },
+    tooltip: {
+      enabled: false,
+      intersect: true,
+      shared: false,
+      theme: 'dark',
+      x: {
+        show: false,
+      },
+      y: {
+        title: { formatter: () => '' },
+      },
+      custom: ({ series, seriesIndex, dataPointIndex, w }) => {
+        const value = series[seriesIndex][dataPointIndex];
+        if (value === null) {
+          return '';
+        }
+        return `<div class="custom-tooltip-container">
+                <div class="custom-tooltip-value">${value}점</div>
+            </div>`;
+      },
+      marker: { show: false },
+      style: { fontSize: '12px' },
+      fillSeriesColor: false,
+      onDatasetHover: { highlightDataSeries: false },
+    },
+    states: {
+      hover: {
+        filter: {
+          type: 'none',
+        },
+      },
+      active: {
+        filter: {
+          type: 'none',
+        },
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'straight',
+      width: 2,
+    },
+    fill: {
+      type: 'solid',
+    },
+    markers: {
+      size: 5,
+      colors: ['#796AFF'],
+      strokeColors: 'none',
+      strokeWidth: 2,
+    },
+    title: {
+      align: 'left',
+      style: {
+        fontSize: '12px',
+        fontWeight: 'bold',
+        color: '#292D32',
+      },
+    },
+    grid: {
+      borderColor: '#707885',
+      xaxis: {
+        lines: {
+          show: false,
+        },
+      },
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      row: {
+        colors: ['transparent', 'transparent'],
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      type: 'numeric',
+      min: 0,
+      max: displayLabels.length - 1,
+      tickAmount: displayLabels.length - 1,
+      crosshairs: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+      labels: {
+        show: true,
+        formatter: (value) => {
+          const index = Math.round(Number(value));
+          return displayLabels[index] || '';
+        },
+        style: {
+          colors: '#707885',
+        },
+      },
+    },
+    yaxis: {
+      min: 0,
+      max: 100,
+      tickAmount: 4,
+      labels: {
+        formatter: (val) => {
+          const labelMap: { [key: number]: string } = {
+            0: '아예없음',
+            25: '조금 있음',
+            50: '적당함',
+            75: '조금 심함',
+            100: '매우 심함',
+          };
+          return labelMap[val];
+        },
+        style: {
+          colors: '#707885',
+        },
+      },
+    },
+  };
+  return (
+    <section className="px-6 py-7 flex flex-col gap-4 bg-gray-800 rounded-[20px]">
+      <div className="flex flex-col">
+        <div className="text-body3-m text-gray-600 mb-2">
+          스트레스 분석 결과
+        </div>
+        <div className="flex justify-center gap-12">
+          <div className="text-h4">{stressAnalysis.message}</div>
+          {/* NOTE: props로 받은 'image' URL로 교체 필요. */}
+          <Image
+            src={streching}
+            alt="스트레스 분석 결과 이미지"
+            width={80}
+            height={80}
+          />
+        </div>
+        {/* 꺾은선 그래프 영역 */}
+        <ApexChart options={options} series={series} type="line" height={350} />
+      </div>
+    </section>
+  );
+}
