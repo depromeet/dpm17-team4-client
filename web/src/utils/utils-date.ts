99c94cc3a6118b56a99162c0dbd6627290349240
SeieunYoo <isho48239@gmail.com>
2025-11-08 14:23:23 +0900

    feat: 코드리뷰

--- web/src/utils/utils-date.ts
+++ web/src/utils/utils-date.ts
@@ -188,5 +188,8 @@ export const getKoreanDate = (): Date => {
  * formatToISOString(new Date(2025, 9, 1)) // "2025-10-01"
  */
 export const formatToISOString = (date: Date): string => {
-  return date.toISOString().split('T')[0];
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
 };
