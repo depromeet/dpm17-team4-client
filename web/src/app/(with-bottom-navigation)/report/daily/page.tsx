ead27d12e5b9cf3841b1ca88ce7b82458a9a7121
ppinppini <bnm0218@naver.com>
2025-11-10 21:20:09 +0900

    fix(web):dvh 삭제

--- web/src/app/(with-bottom-navigation)/report/daily/page.tsx
+++ web/src/app/(with-bottom-navigation)/report/daily/page.tsx
@@ -143,7 +143,7 @@ function DailyReportContent() {
   };
 
   return (
-    <div className="flex flex-col flex-1 h-dvh">
+    <div className="flex flex-col flex-1">
       {/* 날짜 네비게이션 */}
       <div className="flex items-center justify-center gap-4 px-4 py-4 mt-4 mb-2">
         {/* TODO(seonghyun): 다른 날짜 이동 */}
