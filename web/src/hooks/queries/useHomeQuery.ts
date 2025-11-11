f35315979bebc72957333ffd0a95f2331cd0953a
ppinppini <bnm0218@naver.com>
2025-11-11 16:10:13 +0900

    feat(web): home api query

--- web/src/hooks/queries/useHomeQuery.ts
+++ web/src/hooks/queries/useHomeQuery.ts
@@ -1,15 +1,17 @@
-import { homeDataApi } from "@/apis/homeApi";
-import { HomeResponseData, HomeResponseDto } from "@/app/(with-bottom-navigation)/home/types/dto";
-import { QUERY_KEYS } from "@/constants";
-import { useQuery } from "@tanstack/react-query";
-
+import { useQuery } from '@tanstack/react-query';
+import { homeDataApi } from '@/apis/homeApi';
+import {
+  type HomeResponseData,
+  HomeResponseDto,
+} from '@/app/(with-bottom-navigation)/home/types/dto';
+import { QUERY_KEYS } from '@/constants';
 
 export const useGetHomeQuery = (date: string) => {
   return useQuery<HomeResponseData>({
     queryKey: QUERY_KEYS.HOME,
     queryFn: async () => {
-      const response = await homeDataApi.getHomeData(date)
-      return response.data
+      const response = await homeDataApi.getHomeData(date);
+      return response.data;
     },
     refetchOnMount: 'always',
   });
