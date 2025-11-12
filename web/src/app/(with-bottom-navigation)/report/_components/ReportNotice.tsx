a8b7741e12d1f5cea035113fb3bd274cbda58aed
dvp-tae <th3016@naver.com>
2025-11-12 16:42:55 +0900

    feat(web): 건강 정보 출처 링크 연결

--- web/src/app/(with-bottom-navigation)/report/_components/ReportNotice.tsx
+++ web/src/app/(with-bottom-navigation)/report/_components/ReportNotice.tsx
@@ -1,4 +1,10 @@
 const ReportNotice = () => {
+  // NOTE(taehyeon): 생활 습관 및 일반 건강 관련 정보 참고 URL (현재는 앱 컨텍스트 내에서 열림 -> 외부 브라우저로 열려야 하는지 여부는 논의 필요)
+  const linkedURL = 'https://www.snuh.org/health/nMedInfo/nList.do';
+  const handleLinkClick = () => {
+    window.open(linkedURL, '_blank');
+  };
+
   return (
     <div className="pt-[36px] pb-[60px] flex flex-col space-y-5 w-[calc(100%-32px)] mx-auto z-10">
       <p className="text-body2-sb text-[#707885]">유의사항</p>
@@ -34,9 +40,33 @@ const ReportNotice = () => {
           </span>
         </li>
         <br />
-        <li className="flex items-center gap-2">
+        <li className="flex items-center gap-2 h-6">
           <span className="text-[#4E5560] flex-shrink-0">•</span>
           <span>생활 습관 및 일반 건강 관련 정보 참고</span>
+          <button type="button" onClick={handleLinkClick}>
+            <svg
+              role="img"
+              aria-label="info"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                x="5.32422"
+                y="5.32422"
+                width="13.3535"
+                height="13.3535"
+                rx="2"
+                fill="#4E5560"
+              />
+              <path
+                d="M15.1611 13.9622C15.1611 14.1921 14.9748 14.3783 14.745 14.3783C14.5152 14.3783 14.3289 14.192 14.3289 13.9622V10.2586L9.54634 15.0412C9.38383 15.2037 9.12033 15.2037 8.95781 15.0412C8.79532 14.8787 8.7953 14.6152 8.95781 14.4527L13.7404 9.67011H10.0368C9.80696 9.67009 9.62067 9.48383 9.62067 9.254C9.62069 9.02418 9.80697 8.8379 10.0368 8.83789H14.745C14.7701 8.83789 14.7947 8.84052 14.8186 8.84479C14.8479 8.85004 14.8769 8.85811 14.9048 8.86969C14.9326 8.88129 14.9589 8.8959 14.9833 8.91299C15.0029 8.92673 15.0217 8.94221 15.0393 8.95973C15.057 8.97751 15.0729 8.99652 15.0868 9.01644C15.1032 9.03995 15.1172 9.06522 15.1285 9.09192L15.1308 9.09729C15.137 9.11258 15.1407 9.12862 15.145 9.14442C15.1546 9.17945 15.1611 9.21593 15.1611 9.254V13.9622Z"
+                fill="#99A1B1"
+              />
+            </svg>
+          </button>
         </li>
       </ul>
     </div>
