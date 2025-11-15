478fbe47b3d31c5a3cb00fbdf0d2416d9e31fd8e
s-hyun-lee <s.hyun.lee@webtoonscorp.com>
2025-11-15 10:23:37 +0900

    fix(app): 로딩화면

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, AppState, Linking } from 'react-native';
+import { StyleSheet, AppState, Linking, ActivityIndicator, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
 import { useEffect, useRef, useState } from 'react';
@@ -190,6 +190,13 @@ export default function App() {
           originWhitelist={['*']}
           thirdPartyCookiesEnabled={true}
           sharedCookiesEnabled={true}
+          renderLoading={() => (
+            <View style={styles.loadingContainer}>
+              <View style={styles.loadingWrapper}>
+                <ActivityIndicator size="large" color="#ffffff" />
+              </View>
+            </View>
+          )}
         />
         <StatusBar style="auto" />
       </SafeAreaView>
@@ -205,5 +212,19 @@ const styles = StyleSheet.create({
   webview: {
     flex: 1,
     backgroundColor: '#000000',
+  },
+  loadingContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#000000',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
